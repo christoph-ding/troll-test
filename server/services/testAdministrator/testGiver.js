be7d5e947d837e4958c5ff7e16267bc7b12a1a53
Pokegear <christoph.ding@gmail.com>
2016-10-13 15:40:43 -0400

    Added test data files.

--- server/services/testAdministrator/testGiver.js
+++ server/services/testAdministrator/testGiver.js
@@ -3,6 +3,18 @@ var testFunction = function(req, res) {
   res.send('heya!  you just made a get request to get the test\n');
 }
 
+var chooseDataSet = function(req, res, next) {
+
+}
+
+var selectExcludedWords = function(req, res, next) {
+
+}
+
+var createJSONResponse = function(req, res, next) {
+
+}
+
 module.exports = {
   testFunction: testFunction
 }
