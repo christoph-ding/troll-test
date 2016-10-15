00e7bb303e652e47ea2d7a237ab6122ab2ba379a
Pokegear <christoph.ding@gmail.com>
2016-10-15 18:27:34 -0400

    Finally made callbacks work with closures and not promises.

--- server/routers/testRouter.js
+++ server/routers/testRouter.js
@@ -4,8 +4,7 @@ var testChecker = require('../services/testAdministrator/testChecker.js');
 module.exports = function(express) {
   var testRouter = express.Router();
 
-  testRouter.get('/', testGiver.getTest);
-  
+  testRouter.get('/', testGiver.getTest);  
   testRouter.post('/', testChecker.testFunction);
 
   return testRouter;
